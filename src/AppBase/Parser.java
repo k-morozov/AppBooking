a82aa877a2510e1dd082f3213462e4d4572729dc
Konstantin Morozov <morozov-kst@yandex.ru>
2020-11-19 07:22:58 +0300

    update

--- src/AppBase/Parser.java
+++ src/AppBase/Parser.java
@@ -7,6 +7,7 @@ import AppBase.Message.Response.CheckResponse;
 import AppBase.Message.Response.Response;
 import java.util.Iterator;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Parser {
 
@@ -15,41 +16,33 @@ public class Parser {
     }
 
     public ArrayList<Request> parse_request(String str_command) {
-        // System.out.println("get str: " + str_command);
-        ArrayList<Request> req = new ArrayList<>();
+        ArrayList<Request> requests = new ArrayList<>();
 
-        String[] strs = str_command.split(" ");
-        ArrayList<String> params = new ArrayList<>();
-        for(String str : strs) {
-            params.add(str);
-        }
-
+        ArrayList<String> params = new ArrayList<>(Arrays.asList(str_command.split(" ")));
         if (!params.isEmpty()) {
-            switch (params.get(0)) {
+            Iterator<String> itParams = params.iterator();
+
+            switch (itParams.next()) {
                 case "add": {
                     System.out.println("command add");
 
-                    Iterator<String> itParams = params.iterator();
-                    itParams.next();
                     Integer date;
                     if (itParams.hasNext()) {
                         date = Integer.parseInt(itParams.next());
                         while (itParams.hasNext()) {
-                            req.add(AddRequest.create(date, itParams.next()));
+                            requests.add(AddRequest.create(date, itParams.next()));
                         }
                     }
                     break;
                 }
                 case "check": {
                     System.out.println("command check");
-                    // req.add(CheckRequest.create(Integer.parseInt(params.get(1)), params.get(2)));
-                    Iterator<String> itParams = params.iterator();
-                    itParams.next();
+
                     Integer date;
                     if (itParams.hasNext()) {
                         date = Integer.parseInt(itParams.next());
                         while (itParams.hasNext()) {
-                            req.add(CheckRequest.create(date, itParams.next()));
+                            requests.add(CheckRequest.create(date, itParams.next()));
                         }
                     }
                     break;
@@ -60,7 +53,7 @@ public class Parser {
             }
         }
 
-        return req;
+        return requests;
     }
 
     public ArrayList<String> parse_response(ArrayList<Response> responses) {
